9a23e852f3def7cd5a57032e47d96917bf2262de
Knyyyyy <nayeon2421@naver.com>
2025-11-27 14:29:39 +0900

    Create firebaseClient.js

---
+++ 프론트엔드/web/src/firebaseClient.js
@@ -0,0 +1,15 @@
+// client-side Firebase config (only for Firestore realtime if needed)
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
