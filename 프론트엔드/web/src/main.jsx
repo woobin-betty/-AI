13290e2eb8e9c7ddd8a9217774fa7895dabf542a
woobin-betty <ubin060720@gmail.com>
2025-12-02 19:05:27 +0900

    Implement saveAssignment function for Firestore
    Added saveAssignment function to save assignments to Firestore.

--- 프론트엔드/web/src/main.jsx
+++ 프론트엔드/web/src/main.jsx
@@ -3,3 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(<App />);
+
+import { db, collection, addDoc } from "./firebase.js";
+
+async function saveAssignment(title, deadline, description) {
+  await addDoc(collection(db, "assignments"), {
+    title,
+    deadline,
+    description,
+  });
+
+  alert("과제 저장 완료!");
+}
+
+export { saveAssignment };
